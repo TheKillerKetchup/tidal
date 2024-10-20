a8959a16321763aa78f6dcc7666bc114ff71fdb0
Edwin Tan <edwintan7962@gmail.com>
2024-10-20 02:17:29 -0500

    modeling
    5 trainings

---
+++ convert.py
@@ -0,0 +1,6 @@
+from ultralytics.data.converter import convert_coco
+
+convert_coco(
+    labels_dir=r'c:\Users\edwin\Desktop\TACO\data', 
+    use_segments=True
+)
---
+++ training.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import comet_ml
+
+
+if __name__ == "__main__":
+    comet_ml.login(api_key="Ghhy4MCuIlxwIteUDKJ3yfAni", project_name="tidal",workspace="edwta")
+
+    # Load a model
+    model = YOLO("runs/detect/train4/weights/last.pt")
+
+    # Train the model
+    train_results = model.train(
+        data="taco.yaml",  # path to dataset YAML
+        epochs=25,  # number of training epochs
+        imgsz=640  # training image size
+    )
+
+    # Export the model to pt format
+    path = model.export()  # return path to exported model
